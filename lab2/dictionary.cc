63387d8d4b5a67b32552f4afe8aed5f3f63dc527
Anton Roth <antonrothus@gmail.com>
2017-02-03 08:42:53 +0100

    Implemented dictionary constructor.

--- lab2/dictionary.cc
+++ lab2/dictionary.cc
@@ -5,10 +5,23 @@
 #include <algorithm>
 #include "word.h"
 #include "dictionary.h"
+#include <sstream>
 
 using namespace std;
 
 Dictionary::Dictionary() {
+	stringstream ss;
+	string word;
+	ifstream read_stream("words.txt");
+	unsigned int counter = 0;
+	while(getline(read_stream, word, '\n')) {
+		word = word.substr(0, word.find_first_of(" "));
+		//cout << "Word: " << word << endl;
+		//ss >> word;
+		words.insert(word);
+		++counter;
+		//if(counter > 20) break;
+	}
 }
 
 bool Dictionary::contains(const string& word) const {
